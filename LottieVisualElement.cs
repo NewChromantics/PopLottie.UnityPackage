671b3066161dd2100fb97adb76451de3c1307875
Graham Reeves <graham@grahamreeves.com>
2024-05-08 11:14:53 +0100

    Removed Codice.Client.BaseCommands import (where did this come from?! rider!)

--- LottieVisualElement.cs
+++ LottieVisualElement.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Codice.Client.BaseCommands;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
